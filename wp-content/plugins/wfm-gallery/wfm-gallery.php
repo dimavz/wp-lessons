9050a2307c57988a100bd5ab346f146125691834
Дмитрий <zatulenko@gmail.com>
2021-01-16 16:29:32 +0300

    Внесены правки в плагина для вывода Галлереи. Часть 2

--- wp-content/plugins/wfm-gallery/wfm-gallery.php
+++ wp-content/plugins/wfm-gallery/wfm-gallery.php
@@ -9,6 +9,17 @@ Author URI: http://webformyself.com
 
 remove_shortcode( 'gallery' );
 add_shortcode( 'gallery', 'wfm_gallery' );
+add_action( 'wp_enqueue_scripts', 'wfm_styles_scripts_gallery' );
+
+function wfm_styles_scripts_gallery(){
+	wp_register_script( 'wfm-lightbox-js', plugins_url( 'js/lightbox.min.js', __FILE__ ), array('jquery') );
+	wp_register_style( 'wfm-lightbox', plugins_url( 'css/lightbox.css', __FILE__ ) );
+	wp_register_style( 'wfm-lightbox-style', plugins_url( 'css/lightbox-style.css', __FILE__ ) );
+
+	wp_enqueue_script( 'wfm-lightbox-js' );
+	wp_enqueue_style( 'wfm-lightbox' );
+	wp_enqueue_style( 'wfm-lightbox-style' );
+}
 
 function wfm_gallery($atts){
 	$img_id = explode(',', $atts['ids']);
