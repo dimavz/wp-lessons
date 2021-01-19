a5eb7edab10fbf84bbb2f3939b286b1d110e397e
Дмитрий <zatulenko@gmail.com>
2021-01-19 11:27:54 +0300

    Внесены правки в плагин для группировки опций

--- wp-content/plugins/wfm-options/wfm-options.php
+++ wp-content/plugins/wfm-options/wfm-options.php
@@ -11,7 +11,7 @@ Author URI: http://webformyself.com
 // if( update_option( 'wfm_test', 222 ) ) echo 'OK';
 // if( delete_option( 'wfm_test' ) ) echo 'OK';
 
-add_action( 'admin_init', 'wfm_first_option' );
+/*add_action( 'admin_init', 'wfm_first_option' );
 
 function wfm_first_option(){
 	// $option_group, $option_name, $sanitize_callback
@@ -33,6 +33,44 @@ function wfm_option_cb(){
 	?>
 
 <input type="text" name="wfm_first_option" id="wfm_first_option" value="<?php echo esc_attr( get_option('wfm_first_option') ); ?>" class="regular-text">
+
+	<?php
+}*/
+//delete_site_option('wfm_first_option');
+
+add_action( 'admin_init', 'wfm_theme_options' );
+
+function wfm_theme_options(){
+	register_setting( 'general', 'wfm_theme_options' );
+
+	// $id - ID секции
+	// $title - заголовок
+	// $callback - callback-функция для генерации HTML-кода
+	// $page - для какой страницы регистрируется секция
+	add_settings_section( 'wfm_theme_section_id', 'Опции темы', 'wfm_theme_options_section_cb', 'general' );
+
+	add_settings_field( 'wfm_theme_options_body', 'Цвет фона', 'wfm_theme_body_cb', 'general', 'wfm_theme_section_id' );
+	add_settings_field( 'wfm_theme_options_header', 'Цвет header', 'wfm_theme_header_cb', 'general', 'wfm_theme_section_id' );
+}
+
+function wfm_theme_options_section_cb(){
+	echo '<p>Описание секции</p>';
+}
+
+function wfm_theme_body_cb(){
+	$options = get_option('wfm_theme_options');
+	?>
+
+<input type="text" name="wfm_theme_options[wfm_theme_options_body]" id="wfm_theme_options_body" value="<?php echo esc_attr($options['wfm_theme_options_body']); ?>" class="regular-text">
+
+	<?php
+}
+
+function wfm_theme_header_cb(){
+	$options = get_option('wfm_theme_options');
+	?>
+
+<input type="text" name="wfm_theme_options[wfm_theme_options_header]" id="wfm_theme_options_header" value="<?php echo esc_attr($options['wfm_theme_options_header']); ?>" class="regular-text">
 
 	<?php
 }
