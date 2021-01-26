2fea0d1e0da3f040e42409017ce04cca8dea9d07
Дмитрий <zatulenko@gmail.com>
2021-01-26 14:05:22 +0300

    Внесены правки в плагин Галлереи

--- wp-content/plugins/wfm-gallery/wfm-gallery.php
+++ wp-content/plugins/wfm-gallery/wfm-gallery.php
@@ -9,9 +9,45 @@ Author URI: http://webformyself.com
 
 remove_shortcode( 'gallery' );
 add_shortcode( 'gallery', 'wfm_gallery' );
-add_action( 'wp_enqueue_scripts', 'wfm_styles_scripts_gallery' );
+add_action( 'wp_enqueue_scripts', 'wfm_add_styles_scripts' );
+add_action( 'admin_init', 'wfm_gallery_options' );
+register_deactivation_hook( __FILE__, 'wfm_gall_delete_options' );
 
-function wfm_styles_scripts_gallery(){
+function wfm_gall_delete_options(){
+	delete_option( 'wfm_gallery_options' );
+}
+
+function wfm_gallery_options(){
+	// $option_group, $option_name, $sanitize_callback
+	register_setting( 'general', 'wfm_gallery_options' );
+
+	// $id, $title, $callback, $page
+	add_settings_section( 'gallery_section_id', 'Опции галереи', '', 'general' );
+
+	// $id, $title, $callback, $page, $section, $args
+	add_settings_field( 'gallery_option_title', 'Название галереи', 'wfm_gallery_title_cb', 'general', 'gallery_section_id' );
+	add_settings_field( 'gallery_option_text', 'Текст при отсутствии картинок', 'wfm_gallery_text_cb', 'general', 'gallery_section_id' );
+}
+
+function wfm_gallery_title_cb(){
+	$options = get_option('wfm_gallery_options');
+	?>
+
+<input type="text" name="wfm_gallery_options[gallery_option_title]" id="gallery_option_title" value="<?php echo esc_attr($options['gallery_option_title']); ?>" class="regular-text">
+
+	<?php
+}
+
+function wfm_gallery_text_cb(){
+	$options = get_option('wfm_gallery_options');
+	?>
+
+<input type="text" name="wfm_gallery_options[gallery_option_text]" id="gallery_option_text" value="<?php echo esc_attr($options['gallery_option_text']); ?>" class="regular-text">
+
+	<?php
+}
+
+function wfm_add_styles_scripts(){
 	wp_register_script( 'wfm-lightbox-js', plugins_url( 'js/lightbox.min.js', __FILE__ ), array('jquery') );
 	wp_register_style( 'wfm-lightbox', plugins_url( 'css/lightbox.css', __FILE__ ) );
 	wp_register_style( 'wfm-lightbox-style', plugins_url( 'css/lightbox-style.css', __FILE__ ) );
@@ -22,9 +58,10 @@ function wfm_styles_scripts_gallery(){
 }
 
 function wfm_gallery($atts){
+	$options = get_option( 'wfm_gallery_options' );
 	$img_id = explode(',', $atts['ids']);
-	if( !$img_id[0] ) return '<div class="wfm-gallery">В галерее нет картинок</div>';
-	$html = '<div class="wfm-gallery">';
+	if( !$img_id[0] ) return '<div class="wfm-gallery"><h3>' . $options['gallery_option_title'] . '</h3>' . $options['gallery_option_text'] . '</div>';
+	$html = '<div class="wfm-gallery"><h3>' . $options['gallery_option_title'] . '</h3>';
 	foreach($img_id as $item){
 		$img_data = get_posts( array(
 			'p' => $item,
