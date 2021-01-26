ed663f04dfa16886642d8cb63f9100df6029c957
Дмитрий <zatulenko@gmail.com>
2021-01-27 00:38:47 +0300

    Создание собственной страницы опций. Часть 2

--- wp-content/plugins/wfm-options-page/wfm-options-page.php
+++ wp-content/plugins/wfm-options-page/wfm-options-page.php
@@ -7,33 +7,36 @@ Author: Андрей
 Author URI: http://webformyself.com
 */
 
-/*
- * Ссылки по теме:
- * https://wp-kama.ru/function/current_user_can#bazovyj-spisok-prav  объяснение прав параметра capability
- * https://wp-kama.ru/function-cat/admin-panel - функции для добавления страниц а админ-панели
- * */
-
 add_action( 'admin_menu', 'wfm_admin_menu' );
 
 function wfm_admin_menu(){
 	// $page_title, $menu_title, $capability, $menu_slug, $function
 	add_options_page( 'Опции темы (title)', 'Опции темы', 'manage_options', 'wfm-theme-options', 'wfm_option_page' );
-//	add_dashboard_page('Опции темы (title)', 'Опции темы', 'manage_options', 'wfm-theme-options', 'wfm_option_page');
-//	add_theme_page('Опции темы (title)', 'Опции темы', 'manage_options', 'wfm-theme-options', 'wfm_option_page');
 }
 
 function wfm_option_page(){
-//    echo "Привет,Мир!";
-	if( isset($_POST['wfm_hidden']) && $_POST['wfm_hidden'] == 'wfm_hidden' ){
-		print_r($_POST);
+	if( !empty($_POST) && check_admin_referer('wfm-theme-options') ){
+		update_option( 'wfm_theme_options', array(
+				'wfm_theme_options_body' => $_POST['wfm_theme_options']['wfm_theme_options_body'],
+				'wfm_theme_options_header' => $_POST['wfm_theme_options']['wfm_theme_options_header']
+			)
+		);
+		echo '<div class="updated"><p><strong>Настройки сохранены.</strong></p></div>';
 	}
+	$options = get_option( 'wfm_theme_options' );
 	?>
 
 <div class="wrap">
 	<h2>Опции темы</h2>
 	<p>Настройки темы плагина Options &amp; Settings API</p>
 	<form action="" method="post">
-		<input type="hidden" name="wfm_hidden" value="wfm_hidden">
+		<?php wp_nonce_field('wfm-theme-options');
+		// Получает или выводит скрытое одноразовое поле (nonce) для формы.
+		// Одноразовое поле (nonce) нужно для проверки передаваемых данных формы,
+        // чтобы убедиться, что данные были отправлены с текущего сайта, а не от куда-то еще.
+        // Такое поле не дает полной защиты, но защищает в большинстве случаев.
+        // Использовать проверочное поле в формах обязательно!
+        ?>
 		<p>
 			<label for="wfm_theme_options_body">Цвет фона</label>
 			<input type="text" name="wfm_theme_options[wfm_theme_options_body]" id="wfm_theme_options_body" value="<?php echo esc_attr($options['wfm_theme_options_body']); ?>" class="regular-text">
