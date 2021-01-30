9a7fc97a84e96380ead0436ec685f8463d018a7e
Дмитрий <zatulenko@gmail.com>
2021-01-30 15:45:11 +0300

    Работа с AJAX в WordPress. Часть 4

--- wp-content/plugins/wfm-ajax/wfm-ajax.php
+++ wp-content/plugins/wfm-ajax/wfm-ajax.php
@@ -7,27 +7,30 @@ Author: Андрей
 Author URI: http://webformyself.com
 */
 
-/*if( isset($_POST['test']) ){
-	print_r($_POST);
-	die;
-}*/
-
 add_action( 'admin_menu', 'wfm_admin_menu' );
 add_action( 'admin_init', 'wfm_admin_settings' );
-add_action( 'wp_ajax_wfm_action', 'wfm_ajax_check' ); // Хук wp_ajax_wfm_action состоит из двух частей. 1-я часть wp_ajax_ стандартная часть WordPrees
- // 2-я часть wfm_action динамичная часть WordPrees, которая указывается в скрипте при отправке данных data (параметр action)
+add_action( 'wp_ajax_wfm_action', 'wfm_ajax_check' );
 
 function wfm_ajax_check(){
-	if( isset($_POST['formData']) ){
-		update_option( 'wfm_theme_options', $_POST['formData'] );
-		echo 'OK';
+	if( !wp_verify_nonce( $_POST['security']['nonce'], 'wfmajax' ) ){
+		die('NO');
 	}
-	die;
+	if( preg_match("/(^#[a-z0-9]{3}|^#[a-z0-9]{6})$/i", $_POST['formData']) ){ //Регулярное выражение ищет либо 3 символа после знака #, либо 6 символов после знака #
+        if( isset($_POST['formData']) ){
+            update_option( 'wfm_theme_options', $_POST['formData'] );
+            echo 'Данные обновлены';
+        }
+//		echo 'Подходит';
+	}else{
+		echo 'Не верный формат данных';
+	}
+	die();
 }
 
 function wfm_admin_scripts(){
 	wp_register_script( 'wfm-scripts', plugins_url( 'wfm-scripts.js', __FILE__ ), array('jquery') );
 	wp_enqueue_script( 'wfm-scripts' );
+	wp_localize_script( 'wfm-scripts', 'wfmajax', array( 'nonce' => wp_create_nonce('wfmajax') ) );
 }
 
 function wfm_admin_settings(){
@@ -47,7 +50,7 @@ function wfm_theme_body_cb(){
 	$options = get_option( 'wfm_theme_options' );
 	?>
 <p>
-	<input type="text" name="wfm_theme_options" id="wfm_theme_options_body" value="<?php echo esc_attr($options); ?>" class="regular-text">
+	<input type="text" name="wfm_theme_options" id="wfm_theme_options_body" value="<?php echo esc_attr($options); ?>" class="regular-text"> <span id="loader" style="display: none;"><img src="<?php echo plugins_url( 'loader.gif', __FILE__ ); ?>" alt=""></span><span id="res"></span>
 </p>
 	<?php
 }
--- wp-content/plugins/wfm-ajax/wfm-scripts.js
+++ wp-content/plugins/wfm-ajax/wfm-scripts.js
@@ -1,16 +1,31 @@
 jQuery(document).ready(function($){
 
+	$('#submit').attr('disabled', true);
+	$('#wfm_theme_options_body').blur(function(){
+		if( $(this).val() != '' ){
+			$('#submit').attr('disabled', false);
+		}else{
+			$('#submit').attr('disabled', true);
+		}
+	});
+
 	$('#wfm-form').submit(function(){
-		// var formData = $('#wfm-form').serialize()
 		$.ajax({
 			type: "POST",
 			data: {
 				formData: $('#wfm_theme_options_body').val(),
-				action: 'wfm_action' // Используется для формирования хука для WordPress
+				security: wfmajax,
+				action: 'wfm_action'
 			},
 			url: ajaxurl,
+			beforeSend: function(){
+				$('#res').empty();
+				$('#loader').fadeIn();
+			},
 			success: function(res){
-				alert(res);
+				$('#loader').fadeOut(300, function(){
+					$('#res').text(res);
+				});
 			},
 			error: function(){
 				alert('Error!');
@@ -18,20 +33,5 @@ jQuery(document).ready(function($){
 		});
 		return false;
 	});
-
-	/*$('#wfm-form').submit(function(){
-		$.ajax({
-			type: "POST",
-			data: {'test': 'TEST'},
-			url: 'http://plugins.loc/wp-content/plugins/wfm-ajax/wfm-ajax.php',
-			success: function(res){
-				alert(res);
-			},
-			error: function(){
-				alert('Error!');
-			}
-		});
-		return false;
-	});*/
 
 });
