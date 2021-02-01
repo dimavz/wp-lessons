bef3d4028142b706a3ee0a1b1eb674c0a1fcee6e
Дмитрий <zatulenko@gmail.com>
2021-02-01 12:44:38 +0300

    Создание плагина Подписки. Часть 5

--- wp-content/plugins/wfm-subscriber/wfm-subscriber.php
+++ wp-content/plugins/wfm-subscriber/wfm-subscriber.php
@@ -14,6 +14,7 @@ register_activation_hook( __FILE__, 'wfm_create_table' );
 add_action( 'widgets_init', 'wfm_widget_subscriber' );
 add_action( 'wp_ajax_wfm_subscriber', 'wfm_ajax_subscriber' );
 add_action( 'wp_ajax_nopriv_wfm_subscriber', 'wfm_ajax_subscriber' );
+add_action( 'admin_menu', 'wfm_admin_menu' );
 
 function wfm_create_table(){
 	global $wpdb;
@@ -34,4 +35,27 @@ function wfm_subscriber_scripts(){
 	wp_register_script( 'wfm-subscriber', plugins_url( 'js/wfm-subscriber.js', __FILE__ ), array('jquery') );
 	wp_enqueue_script( 'wfm-subscriber' );
 	wp_localize_script( 'wfm-subscriber', 'wfmajax', array( 'url' => admin_url( 'admin-ajax.php' ), 'nonce' => wp_create_nonce( 'wfmajax' ) ) );
+}
+
+function wfm_admin_menu(){
+	add_options_page( 'Подписчики', 'Подписчики', 'manage_options', 'wfm-subscriber', 'wfm_subscriber_page' );
+	add_action( 'admin_enqueue_scripts', 'wfm_admin_scripts' );
+}
+
+function wfm_admin_scripts($hook){
+//    echo '<pre>';
+//    print_r($hook);
+//    echo '</pre>';
+//    exit();
+	if( $hook != 'settings_page_wfm-subscriber' ) return;
+	wp_enqueue_style( 'wfm-style', plugins_url( 'css/wfm-subscriber.css', __FILE__ ) );
+	wp_enqueue_script( 'wfm-admin-scripts', plugins_url( 'js/wfm-subscriber-admin.js', __FILE__ ), array('jquery'),false,true );
+}
+
+function wfm_subscriber_page(){
+	?>
+<div class="wrap">
+	<h2>Подписчики</h2>
+</div>
+	<?php
 }
