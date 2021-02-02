7976d0ed596a8e7d854f383be9ae9b53faa6f198
Дмитрий <zatulenko@gmail.com>
2021-02-02 12:55:11 +0300

    Работа с Cron в WordPress. Работа с расписанием. Часть 2 Добавление собственных интервалов в расписание

--- wp-content/plugins/wfm-cron/wfm-cron.php
+++ wp-content/plugins/wfm-cron/wfm-cron.php
@@ -7,23 +7,33 @@ Author: Андрей
 Author URI: http://webformyself.com
 */
 
-//wp_clear_scheduled_hook('wfm_cron_action');
+// wp_clear_scheduled_hook( 'wfm_cron_action' );
 
-add_action('wfm_cron_action', 'wfm_cron_cb');
+add_action( 'init', 'wfm_setup_schedule' );
+add_action( 'wfm_cron_action', 'wfm_cron_cb' );
+add_filter( 'cron_schedules', 'wfm_add_schedules' );
 
-if (!wp_next_scheduled('wfm_cron_action')) {
-    wp_schedule_event(time(), 'hourly', 'wfm_cron_action');
+// $schedules = wp_get_schedules();
+// print_r($schedules);
+
+function wfm_setup_schedule(){
+	if( !wp_next_scheduled( 'wfm_cron_action' ) ){
+		wp_schedule_event( time(), 'minute', 'wfm_cron_action' );
+	}
 }
 
-function wfm_cron_cb()
-{
-    error_log('Cron worked ### ' . date("i:s", time()));
+function wfm_cron_cb(){
+	error_log("Cron worked " . date("i:s", time()));
 }
 
-//add_action( 'wfm_cron_action', 'wfm_cron_cb' );
-//
-//wp_schedule_event( time(), 'hourly', 'wfm_cron_action' );
-//
-//function wfm_cron_cb(){
-//	error_log( 'Крон отработал ' . date("h:i:s", time()) );
-//}
+function wfm_add_schedules($schedules){ // Добавляем собственные интервалы в расписание
+	$schedules['minute'] = array(
+		'interval' => 60,
+		'display' => 'Every 1 minute'
+	);
+	$schedules['once-two-minutes'] = array(
+		'interval' => 120,
+		'display' => 'Every 2 minutes'
+	);
+	return $schedules;
+}
