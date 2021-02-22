41ebb60580334408c2a368ad881c15f9396540e8
Дмитрий <zatulenko@gmail.com>
2021-02-22 16:10:15 +0300

    Создание темы для WordPress. Урок 8.  Цикл The Loop в WordPress

--- wp-content/themes/mytesttheme/index.php
+++ wp-content/themes/mytesttheme/index.php
@@ -1,5 +1,15 @@
+<?php //get_header('test'); ?>
 <?php get_header(); ?>
 
-<h1>Hello, world!</h1>
+    <h1>Hello, world!</h1>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <!-- post -->
+        <?php the_title('<h4>','</h4>'); ?>
+    <?php endwhile; ?>
+    <!-- post navigation -->
+<?php else: ?>
+    <!-- no posts found -->
+    <p>Постов нет...</p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
