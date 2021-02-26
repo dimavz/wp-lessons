621c27d07066d7ecbf92fbb183ba0922a474ef6c
Дмитрий <zatulenko@gmail.com>
2021-02-26 14:43:18 +0300

    Создание темы для WordPress. Урок 17.   Работа с меню в WordPress. Часть 2. Внесены правки в шаблон footer для вывода нижнего меню

--- wp-content/themes/mytesttheme/footer.php
+++ wp-content/themes/mytesttheme/footer.php
@@ -1,5 +1,19 @@
 </div><!-- end .wrapper -->
-<?php wp_nav_menu(array('theme_location'=> 'footer_menu')) ;?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse">
+        <?php
+        $args = array(
+//        'menu' => 'Основное меню', // выводится по названию меню. На практике практически не используется
+            'theme_location' => 'footer_menu', // На практиче указывается идентификатор меню, который мы добавляли в функцию register_nav_menus()
+            'container' => false,
+            'menu_class' => 'navbar-nav mr-auto',
+            'walker' => new MyTestTheme_Walker_Nav_Menu,
+        );
+        wp_nav_menu($args);
+        ?>
+    </div>
+</nav>
+
 <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
 <?php wp_footer(); ?>
 
